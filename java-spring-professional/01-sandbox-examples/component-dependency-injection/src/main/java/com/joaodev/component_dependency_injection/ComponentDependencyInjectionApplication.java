f9ef387ecc8a4028c48ef1d7cb38683a7a67a452
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-11-21 17:09:21 -0300

    Criação do projeto ComponentDependencyInjection
    Projeto para aulas sobre injeção de dependências em springboot

---
+++ java-spring-professional/01-sandbox-examples/component-dependency-injection/src/main/java/com/joaodev/component_dependency_injection/ComponentDependencyInjectionApplication.java
@@ -0,0 +1,13 @@
+package com.joaodev.component_dependency_injection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComponentDependencyInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComponentDependencyInjectionApplication.class, args);
+	}
+
+}
