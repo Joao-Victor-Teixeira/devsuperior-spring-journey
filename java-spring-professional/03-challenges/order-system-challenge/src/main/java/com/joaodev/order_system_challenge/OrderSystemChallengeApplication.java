f9f97430031b4d2571c282b1438053c013b4d08d
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-11-21 19:00:25 -0300

    Criação do desafio do primeiro módulo

---
+++ java-spring-professional/03-challenges/order-system-challenge/src/main/java/com/joaodev/order_system_challenge/OrderSystemChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.joaodev.order_system_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSystemChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderSystemChallengeApplication.class, args);
+	}
+
+}
---
+++ java-spring-professional/03-challenges/order-system-challenge/src/test/java/com/joaodev/order_system_challenge/OrderSystemChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaodev.order_system_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSystemChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
