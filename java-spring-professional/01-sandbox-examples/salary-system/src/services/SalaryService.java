1e1032c26098d2a9c2a00a2f7c41c80ee568df6d
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-11-21 11:19:56 -0300

    Implementação de exemplo didático
    Exemplo da aula sobre sistemas e componentes

---
+++ java-spring-professional/01-sandbox-examples/salary-system/src/services/SalaryService.java
@@ -0,0 +1,15 @@
+package services;
+
+import entities.Employee;
+
+public class SalaryService {
+
+    // FORMA ERRADA!
+    TaxService taxService = new TaxService();
+    PensionService pensionService = new PensionService();
+
+    public Double netSalary(Employee employee){
+        return employee.getGrossSalary() - taxService.tax(employee.getGrossSalary())
+                - pensionService.discount(employee.getGrossSalary());
+    }
+}
