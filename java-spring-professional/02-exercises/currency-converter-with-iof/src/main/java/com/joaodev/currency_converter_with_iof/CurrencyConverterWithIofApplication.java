affb5b3a3a7592e83bf46701bbdadb2c34fbf59c
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-11-22 12:21:13 -0300

    Cria exercicio 03

---
+++ java-spring-professional/02-exercises/currency-converter-with-iof/src/main/java/com/joaodev/currency_converter_with_iof/CurrencyConverterWithIofApplication.java
@@ -0,0 +1,13 @@
+package com.joaodev.currency_converter_with_iof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConverterWithIofApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConverterWithIofApplication.class, args);
+	}
+
+}
---
+++ java-spring-professional/02-exercises/currency-converter-with-iof/src/test/java/com/joaodev/currency_converter_with_iof/CurrencyConverterWithIofApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaodev.currency_converter_with_iof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConverterWithIofApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
