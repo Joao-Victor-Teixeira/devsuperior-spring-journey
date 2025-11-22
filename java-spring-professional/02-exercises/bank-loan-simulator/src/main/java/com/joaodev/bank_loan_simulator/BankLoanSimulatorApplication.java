9f12139f6c53bb2d8474a03d1f30df70ced5322a
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-11-22 11:31:20 -0300

    Criação do exercicio extra 02
    Cenário: Um banco quer um sistema simples para simular o Valor Final que um cliente vai pagar num empréstimo, considerando os juros mensais. A taxa de juros muda dependendo da duração do empréstimo.

---
+++ java-spring-professional/02-exercises/bank-loan-simulator/src/main/java/com/joaodev/bank_loan_simulator/BankLoanSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.joaodev.bank_loan_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankLoanSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankLoanSimulatorApplication.class, args);
+	}
+
+}
---
+++ java-spring-professional/02-exercises/bank-loan-simulator/src/test/java/com/joaodev/bank_loan_simulator/BankLoanSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaodev.bank_loan_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankLoanSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
