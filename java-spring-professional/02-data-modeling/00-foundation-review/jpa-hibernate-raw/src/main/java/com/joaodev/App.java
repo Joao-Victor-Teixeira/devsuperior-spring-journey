925a75aca36ce5cf0c3ff8803796876e50a3cd9a
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-11-24 18:09:16 -0300

    Conclusão no nivelamento sobre JPA Hibernate
    Código de uma aula do YouTube do canal DevSuperior-
    Introdução JPA e Hibernate (bônus Maven e MySQL) Aulão #006
    
    Servindo como nivelamento pra usar Spring Data e JPA

--- java-spring-professional/02-data-modeling/00-foundation-review/jpa-hibernate-raw/src/main/java/com/joaodev/App.java
+++ java-spring-professional/02-data-modeling/00-foundation-review/jpa-hibernate-raw/src/main/java/com/joaodev/App.java
@@ -1,13 +1,28 @@
 package com.joaodev;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import com.joaodev.dominio.Pessoa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class App {
+   	public static void main(String[] args) {
+		
+		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();
+		
+		System.out.println("Pronto!");
+		em.close();
+		emf.close();
+	}
 }
