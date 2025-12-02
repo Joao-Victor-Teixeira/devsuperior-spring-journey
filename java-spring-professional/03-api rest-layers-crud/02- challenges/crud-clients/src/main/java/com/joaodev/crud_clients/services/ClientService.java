0f986542f5d0bfca90e3ebaf3443fd6adfcfc2fd
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-12-02 18:55:48 -0300

    Deleta clientes com o método DELETE
    Obs: não tem como ter violação de integridade referencial pois não há associações, por isso não foi implementado o tratamento

--- java-spring-professional/03-api rest-layers-crud/02- challenges/crud-clients/src/main/java/com/joaodev/crud_clients/services/ClientService.java
+++ java-spring-professional/03-api rest-layers-crud/02- challenges/crud-clients/src/main/java/com/joaodev/crud_clients/services/ClientService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.joaodev.crud_clients.dto.ClientDTO;
@@ -51,6 +52,14 @@ public class ClientService {
         catch (EntityNotFoundException e) {
             throw new ResourceNotFoundException("Recurso não encontrado");
         }
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id){
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+        repository.deleteById(id);
     }
 
     private void copyDto(ClientDTO dto, Client entity){
