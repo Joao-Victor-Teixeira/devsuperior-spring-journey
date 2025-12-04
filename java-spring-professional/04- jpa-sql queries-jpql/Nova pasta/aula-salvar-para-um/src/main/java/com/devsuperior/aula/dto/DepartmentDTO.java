a021a7d748593106c4e3961201781535684587ba
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-12-04 17:04:51 -0300

    Implementa DTOs aninhados

---
+++ java-spring-professional/04- jpa-sql queries-jpql/Nova pasta/aula-salvar-para-um/src/main/java/com/devsuperior/aula/dto/DepartmentDTO.java
@@ -0,0 +1,36 @@
+package com.devsuperior.aula.dto;
+
+import com.devsuperior.aula.entities.Department;
+
+public class DepartmentDTO {
+
+    private Long id;
+    private String name;
+    
+    public DepartmentDTO(){
+    }
+
+     public DepartmentDTO(Department entity) {
+        id = entity.getId();
+        name = entity.getName();
+    }
+
+
+    public DepartmentDTO(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    
+}
