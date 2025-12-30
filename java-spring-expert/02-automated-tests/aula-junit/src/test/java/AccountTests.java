41c9550940b532886b421a75a52faf3063d71208
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2025-12-30 15:27:17 -0300

    Test com JUnit

---
+++ java-spring-expert/02-automated-tests/aula-junit/src/test/java/AccountTests.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.joaodev.entities.Account;
+
+public class AccountTests {
+
+    @Test
+    public void depositSholdIncreaseBalanceWhenPositiveAmount(){
+
+        double amount = 200.0;
+        double expectedValue = 196.0;
+        Account acc = new Account(1L, 0.0);
+
+        acc.deposit(amount);
+
+        Assertions.assertEquals(expectedValue, acc.getBalance());
+    }
+}
