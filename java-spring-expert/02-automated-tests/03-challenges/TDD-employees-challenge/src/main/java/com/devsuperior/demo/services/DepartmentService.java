d7f1618d10b9d4bf88923f9ffa21ca47b19e5540
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2026-01-02 16:51:03 -0300

    exerciserTDD

---
+++ java-spring-expert/02-automated-tests/03-challenges/TDD-employees-challenge/src/main/java/com/devsuperior/demo/services/DepartmentService.java
@@ -0,0 +1,26 @@
+package com.devsuperior.demo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.demo.dto.DepartmentDTO;
+import com.devsuperior.demo.entities.Department;
+import com.devsuperior.demo.repositories.DepartmentRepository;
+
+@Service
+public class DepartmentService {
+
+    @Autowired
+    private DepartmentRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<DepartmentDTO> findAll(){
+
+        List<Department> result = repository.findAll(Sort.by("name"));
+        return result.stream().map(x -> new DepartmentDTO(x)).toList();
+    }
+}
