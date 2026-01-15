5e7d6617ad00c6eb641744e6ff37b51afe5870ce
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2026-01-15 10:59:51 -0300

    Dsmovie v2

--- java-spring-reference/05-API-versioning/dsmovie-main/backend/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
+++ java-spring-reference/05-API-versioning/dsmovie-main/backend/src/main/java/com/devsuperior/dsmovie/controllers/MovieController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmovie.dto.MovieDTO;
+import com.devsuperior.dsmovie.dto.MovieGenreDTO;
 import com.devsuperior.dsmovie.services.MovieService;
 
 @RestController
@@ -22,9 +23,19 @@ public class MovieController {
 	public Page<MovieDTO> findAll(Pageable pageable) {
 		return service.findAll(pageable);
 	}
+
+	@GetMapping(produces = "application/vnd.devsuperior.dsmovie-v2+json")
+	public Page<MovieGenreDTO> findAllV2(Pageable pageable) {
+		return service.findAllV1(pageable);
+	}
 	
 	@GetMapping(value = "/{id}")
 	public MovieDTO findById(@PathVariable Long id) {
 		return service.findById(id);
+	}
+
+	@GetMapping(value = "/{id}", produces = "application/vnd.devsuperior.dsmovie-v2+json")
+	public MovieGenreDTO findByIdV2(@PathVariable Long id) {
+		return service.findByIdV1(id);
 	}
 }
