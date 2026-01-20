0bbd60d4c2bef88a15ecb3412208ff651b7e088d
JoaoVictorTeixeira <jxvxteixeira777@outlook.com>
2026-01-20 10:55:43 -0300

    Post by id,query methods,query operators

--- java-spring-reference/07-mongoDB/workshop-mongo/src/main/java/com/joaodev/workshop_mongo/repositories/PostRepository.java
+++ java-spring-reference/07-mongoDB/workshop-mongo/src/main/java/com/joaodev/workshop_mongo/repositories/PostRepository.java
@@ -1,9 +1,20 @@
 package com.joaodev.workshop_mongo.repositories;
 
+import java.time.Instant;
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.joaodev.workshop_mongo.models.entities.Post;
 
 public interface PostRepository extends MongoRepository<Post, String> {
 
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ $and: [ { moment: {$gte: ?1} }, { moment: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	List<Post> fullSearch(String text, Instant startMoment, Instant endMoment);
 }
